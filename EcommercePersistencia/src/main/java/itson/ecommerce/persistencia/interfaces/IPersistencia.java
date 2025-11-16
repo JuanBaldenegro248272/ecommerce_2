cb0ffd9b8f47d6584f96015961a658e405c024f8
voiddstles <victoria.vzls24@gmail.com>
2025-11-16 16:51:31 -0700

    resenas

--- EcommercePersistencia/src/main/java/itson/ecommerce/persistencia/interfaces/IPersistencia.java
+++ EcommercePersistencia/src/main/java/itson/ecommerce/persistencia/interfaces/IPersistencia.java
@@ -6,6 +6,8 @@ package itson.ecommerce.persistencia.interfaces;
 
 import itson.ecommerce.persistencia.dtos.NuevoProductoDTO;
 import itson.ecommerce.persistencia.dtos.ProductoListaDTO;
+import itson.ecommerce.persistencia.entidades.EstadoResena;
+import itson.ecommerce.persistencia.entidades.Resena;
 import itson.ecommerce.persistencia.exceptions.PersistenciaException;
 import java.util.List;
 
@@ -20,5 +22,12 @@ public interface IPersistencia {
     public abstract List<ProductoListaDTO> obtenerTodosProductos() throws PersistenciaException;
 
     public abstract List<ProductoListaDTO> buscarProductos(String termino) throws PersistenciaException;
+    
+    public List<Resena> buscarResenas(String termino, EstadoResena estado) throws PersistenciaException;
 
+    public Resena consultarResena(Long id) throws PersistenciaException;
+
+    public Resena actualizarResena(Resena resena) throws PersistenciaException;
+
+    public boolean eliminarResena(Long idResena) throws PersistenciaException;
 }
