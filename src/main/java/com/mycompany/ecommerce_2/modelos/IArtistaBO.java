e633ff1b85d0ee0990bf8c8aec686db6c4f274c7
voiddstles <victoria.vzls24@gmail.com>
2025-11-16 21:41:01 -0700

    maquetado de album

---
+++ src/main/java/com/mycompany/ecommerce_2/modelos/IArtistaBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.ecommerce_2.modelos;
+
+import itson.ecommerce.persistencia.entidades.Artista;
+import java.util.List;
+
+/**
+ *
+ * @author victoria
+ */
+public interface IArtistaBO {
+    List<Artista> consultarTodos();
+}
