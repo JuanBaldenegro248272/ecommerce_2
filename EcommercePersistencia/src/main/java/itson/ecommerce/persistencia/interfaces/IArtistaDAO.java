06ad22a01e0c7a605c640be20f37971b9a094cf4
voiddstles <victoria.vzls24@gmail.com>
2025-11-16 21:21:47 -0700

    maquetado de album

---
+++ EcommercePersistencia/src/main/java/itson/ecommerce/persistencia/interfaces/IArtistaDAO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package itson.ecommerce.persistencia.interfaces;
+
+import itson.ecommerce.persistencia.entidades.Artista;
+import java.util.List;
+
+/**
+ *
+ * @author victoria
+ */
+public interface IArtistaDAO {
+    List<Artista> consultarTodos();
+}
