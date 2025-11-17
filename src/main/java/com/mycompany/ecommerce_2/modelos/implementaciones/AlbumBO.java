a35212c5296430461762f3f818c770f793f00bfe
voiddstles <victoria.vzls24@gmail.com>
2025-11-16 21:41:09 -0700

    maquetado de album

---
+++ src/main/java/com/mycompany/ecommerce_2/modelos/implementaciones/AlbumBO.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ecommerce_2.modelos.implementaciones;
+
+import com.mycompany.ecommerce_2.modelos.IAlbumBO;
+import itson.ecommerce.persistencia.entidades.Album;
+import itson.ecommerce.persistencia.exceptions.PersistenciaException;
+import itson.ecommerce.persistencia.implementaciones.Persistencia;
+import itson.ecommerce.persistencia.interfaces.IPersistencia;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author victoria
+ */
+public class AlbumBO implements IAlbumBO {
+    private final IPersistencia persistencia;
+    private static final Logger LOG = Logger.getLogger(AlbumBO.class.getName());
+
+    public AlbumBO() {
+        this.persistencia = new Persistencia();
+    }
+
+    @Override
+    public List<Album> buscar(String termino) {
+        try {
+            return this.persistencia.buscarAlbumes(termino);
+        } catch (PersistenciaException ex) {
+            LOG.log(Level.SEVERE, "Error al buscar álbumes", ex);
+            return null; 
+        }
+    }
+
+    @Override
+    public Album consultar(Long id) {
+        try {
+            return this.persistencia.consultarAlbum(id);
+        } catch (PersistenciaException ex) {
+            LOG.log(Level.SEVERE, "Error al consultar álbum por ID", ex);
+            return null;
+        }
+    }
+
+    @Override
+    public Album crear(Album album) {
+        try {
+            return this.persistencia.crearAlbum(album);
+        } catch (PersistenciaException ex) {
+            LOG.log(Level.SEVERE, "Error al crear álbum", ex);
+            return null;
+        }
+    }
+
+    @Override
+    public Album actualizar(Album album) {
+        try {
+            return this.persistencia.actualizarAlbum(album);
+        } catch (PersistenciaException ex) {
+            LOG.log(Level.SEVERE, "Error al actualizar álbum", ex);
+            return null;
+        }
+    }
+
+    @Override
+    public boolean eliminar(Long id) {
+        try {
+            return this.persistencia.eliminarAlbum(id);
+        } catch (PersistenciaException ex) {
+            LOG.log(Level.SEVERE, "Error al eliminar álbum", ex);
+            return false;
+        }
+    }
+}
